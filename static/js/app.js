34472f7e3554c5c21de7320f9b654c5b59ffc694
pamcuellas <pamcuellas@gmail.com>
2019-12-03 21:35:50 -0500

    jQuery moved from HTML page to separated file.

---
+++ static/js/app.js
@@ -0,0 +1,20 @@
+      $(document).ready(function(){
+
+      	// Start spinner effect on button when clicked
+        $("#btn_pred").click( function (index) {
+          $("#span_pred").addClass('spinner-border spinner-border-sm');
+        });      
+
+      	// Start spinner effect on button when clicked
+        $("#btn_grab").click( function (index) {
+          $("#span_grab").addClass('spinner-border spinner-border-sm');
+        });    
+
+      	// Tooltip to present info about tweet.
+        $('[data-toggle="tooltip"]').tooltip();   
+
+        // Clean up text boxes
+        $("#clean-up").click( function () {
+			$(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
+        });      
+    });

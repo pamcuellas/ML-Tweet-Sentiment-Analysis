1c6c46554f76a39c137378e66a364f2e69867a74
pamcuellas <pamcuellas@gmail.com>
2019-12-03 21:46:56 -0500

    Adding config.py to Heroku.

---
+++ config.py
@@ -0,0 +1,24 @@
+# ===========================================================================================
+# Twitter Credentials
+# ===========================================================================================
+consumer_key = 'juvQm2wXwBPzPjbMMWNKe8zUH'
+consumer_secret = '5vWR7BdBqONdCUMF0QFOy0N6JV3Otgkm4duPXAGCyR3hLnNPRg'
+access_token = '150047613-2ncjveMTWFKdDjMFf8cFkjM0wfJXS42iqBoBqMXz'
+access_secret = 'bPqaH5RJFdfvZDoEhKPjTzufEhx5KPWEDNY4C97KKldQu'
+
+
+# ===========================================================================================
+# MongoDB Credentials
+# ===========================================================================================
+psswd = "ai_user123"
+dbname = "dbAI"
+dbuser = "ai_user"
+host = "@project2-ha8my.mongodb.net"
+parameters = 'retryWrites=true&w=majority'
+
+#psswd = "7cU7KqmwItQlBJi7"
+#dbname = "dbAI"
+#dbuser = "mongorootuser"
+#host = "@cluster0-mcbfw.mongodb.net"
+#parameters = 'retryWrites=true&w=majority'
+#===========================================================================================
